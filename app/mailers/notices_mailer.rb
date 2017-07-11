29ffc3c89ad5aa9d9ce9a4018084ac39198837ea
Bing Xie <bxie@locomote.com>
2017-07-11 21:54:18 +1000

    New notify to sell email

--- app/mailers/notices_mailer.rb
+++ app/mailers/notices_mailer.rb
@@ -1,2 +1,8 @@
 class NoticesMailer < ApplicationMailer
+  def notify_to_sell(recipient:, symbol:)
+    @recipient = recipient
+    @symbol = symbol
+
+    mail to: recipient.email, subject: "It's time to sell: #{symbol}"
+  end
 end
--- test/mailers/previews/notices_mailer_preview.rb
+++ test/mailers/previews/notices_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notices_mailer
 class NoticesMailerPreview < ActionMailer::Preview
+  User = Struct.new(:email, :name)
 
+  def notify_to_sell
+    user = User.new('bing708@gmail.com', 'Xie Bing')
+
+    NoticesMailer.notify_to_sell(recipient: user, symbol: 'APPL')
+  end
 end
