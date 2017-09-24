fd40c283c75447f0651f6f548ae072a7df1715a9
Bing Xie <bxie@locomote.com>
2017-09-24 16:30:08 +1000

    Refactoring StockService and add tests

--- app/services/stock_service.rb
+++ app/services/stock_service.rb
@@ -1,26 +1,32 @@
 class StockService
   include Concerns::Service
 
+  SELL_ACTION = "sell".freeze
+  HOLD_ACTION = "hold".freeze
+
   attr_reader :long_term_stocks, :yahoo_client
 
-  def initialize
+  def initialize(stocks, yahoo_client)
     #load_up_stocks =
-    @long_term_stocks = { "shop" => 98.0, "tsla" => 347.64 }
-    @yahoo_client = YahooFinance::Client.new
+    @long_term_stocks = stocks
+    @yahoo_client = yahoo_client
   end
 
   def call
     stock_symbols = long_term_stocks.keys
 
-    results = yahoo_client.quotes(stock_symbols, [:bid, :last_trade_price], { na_as_nil: true, raw: false })
+    results = yahoo_client.quotes(stock_symbols, [:bid, :last_trade_price], { na_as_nil: true, raw: true })
 
     results.each do |result|
       target_price = long_term_stocks[result.symbol]
       result.target_price = target_price
-      if result.bid < target_price
-        result.action = "sell"
+
+      next unless result.bid
+
+      if result.bid.to_f < target_price
+        result.action = SELL_ACTION
       else
-        result.action = "hold"
+        result.action = HOLD_ACTION
       end
     end
   end
---
+++ spec/services/sell_stock_notifier_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe SellStockNotifier do
+  subject { described_class.call }
+
+  it "call" do
+    subject
+  end
+end
