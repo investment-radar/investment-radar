b6e19fbfbf40b94a8a6748a2be6ee2bbd9d0ea02
Bing Xie <bing.xie78@gmail.com>
2017-09-27 21:22:19 +1000

    Update stock service spec

--- spec/services/stock_service_spec.rb
+++ spec/services/stock_service_spec.rb
@@ -3,34 +3,38 @@ require 'rails_helper'
 RSpec.describe StockService do
 
   let(:yahoo_client) { YahooFinance::Client.new }
+  let(:yahoo_result) { OpenStruct.new(bid: "350.06", last_trade_price: "351.09", symbol: "tsla") }
+  let(:stockes) { LongTermStock.all }
+
+  subject { described_class.call(stockes, yahoo_client) }
+
+  before do
+    LongTermStock.create(stock_symbol: 'tsla', target_price: 351.64)
+  end
 
   context "when mocks the yahoo client quoties API" do
-    let(:bid_price) { nil }
-    let(:stockes)   { LongTermStock.all }
-
     before do
-      LongTermStock.create(stock_symbol: 'tsla', target_price: 351.64)
-
-      yahoo_result = OpenStruct.new(bid: bid_price, last_trade_price: "351.09", symbol: "tsla")
       allow(yahoo_client).to receive(:quotes).and_return([yahoo_result])
+    end
 
-      described_class.call(stockes, yahoo_client)
+    it 'updates the long term stock record' do
+      expect(subject.size).to eq 1
+      stock = LongTermStock.last
+
+      expect(stock.bid_price).to eq 350.06
+      expect(stock.last_trade_price).to eq 351.09
+      expect(stock.action).to eq 'sell'
     end
   end
 
   context "when call the real yahoo api" do
-    it "returns 2 results" do
-      expect(subject.size).to eq 2
+    it "updates the long term stock record" do
+      expect(subject.size).to eq 1
 
-      shop_result = subject.first
-      expect(shop_result.last_trade_price).not_to be_nil
-      expect(shop_result.symbol).to eq 'shop'
-      expect(shop_result.target_price).to eq 98.0
+      tsla_result = subject.first
 
-      tsla_result = subject.last
       expect(tsla_result.last_trade_price).not_to be_nil
       expect(tsla_result.symbol).to eq 'tsla'
-      expect(tsla_result.target_price).to eq 347.64
     end
   end
 
