6de7f77f9c5d8ee66b52fa89d8eb5b0254f36e5b
Bing Xie <bxie@locomote.com>
2017-09-28 14:46:14 +1000

    Add annotate gem

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ group :development do
   gem 'spring'
   gem 'spring-commands-rspec'
   gem 'spring-watcher-listen', '~> 2.0.0'
+
+  gem 'annotate'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/long_term_stock.rb
+++ app/models/long_term_stock.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: long_term_stocks
+#
+#  id               :integer          not null, primary key
+#  stock_symbol     :string(10)       not null
+#  target_price     :float            not null
+#  bid_price        :float
+#  last_trade_price :float
+#  action           :string(10)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class LongTermStock < ApplicationRecord
   SELL_ACTION = "sell".freeze
   HOLD_ACTION = "hold".freeze
