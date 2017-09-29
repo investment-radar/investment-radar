b4a7bbdd69cce08e0fb222810252ed0467829a09
Bing Xie <bxie@locomote.com>
2017-09-29 10:27:30 +1000

    Add bit log

--- app/mailers/notices_mailer.rb
+++ app/mailers/notices_mailer.rb
@@ -2,11 +2,15 @@ class NoticesMailer < ApplicationMailer
   def notify_to_sell(email:)
     @stocks = LongTermStock.all
 
-    subject = "Stock:"
+    subject = "(#{Rails.env}) Stock:"
 
     @stocks.each do |stock|
       subject += " #{stock.action}-#{stock.stock_symbol.upcase}"
     end
+
+    logger.info "logger Email subject: #{subject}"
+
+    puts "Email subject: #{subject}"
 
     mail to: email, subject: subject
   end
