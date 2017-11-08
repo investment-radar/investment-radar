459326f769e0e99d90f721675323779f771d9c61
Bing Xie <bxie@locomote.com>
2017-11-08 15:05:28 +1100

    Create new stock client

--- lib/finance_client/stock.rb
+++ lib/finance_client/stock.rb
@@ -1,7 +1,22 @@
 module FinanceClient
   class Stock
-    def self.hello
-      p "Hello"
+    BASE_URL = 'https://api.iextrading.com'.freeze
+    API_VERSION = '1.0'.freeze
+
+    def self.quote(symbol, params = nil)
+      quote_url = "/#{API_VERSION}/stock/#{symbol}/quote"
+      response = connection.get quote_url, params
+
+      JSON.parse(response.body) if response&.success?
+    end
+
+    private
+
+    def self.connection
+      conn = Faraday.new(url: BASE_URL) do |faraday|
+        faraday.adapter Faraday.default_adapter
+        # faraday.response :logger # for debug
+      end
     end
   end
 end
--- spec/lib/finance_client/stock_spec.rb
+++ spec/lib/finance_client/stock_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 require './lib/finance_client/stock'
 
 RSpec.describe FinanceClient::Stock do
+  describe '#quote' do
+    subject(:body) { described_class.quote('team') }
 
-  it "" do
-    described_class.hello
+    it { is_expected.to be_kind_of Hash }
+
+    it { is_expected.to include 'symbol' => 'TEAM'}
+
+    it { expect(body["latestPrice"]).to be_kind_of Float }
   end
 end
