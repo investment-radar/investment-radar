f30371d5672262ae6e20242cadde839bf058a200
Bing Xie <bxie@locomote.com>
2017-12-28 11:01:17 +1100

    Fix failed CI

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe 'Welcome Page' do
   it 'visit welcome' do
-    visit '/'
+    visit root_path
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
 
     # expect(page).to have_content 'Hello World'
