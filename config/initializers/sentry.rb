a64f8de7e862fac90008a57c323e75f55e3569d6
Bing Xie <bxie@locomote.com>
2018-01-22 13:18:40 +1100

    Put sentry dsn in secrets

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
-  config.dsn = 'https://24f928b18494442eadb18c63e146a71a:4de04d1c793f4bf780d36becbba4ea46@sentry.io/275207'
+  config.dsn = Rails.application.secrets.sentry_dsn if Rails.application.secrets.sentry_dsn
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  # config.release = APP_VERSION
+  config.environments = ['staging', 'production']
 end
