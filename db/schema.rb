f0e04c5c0077a21d75ac33897b539f5c6fcc7147
Bing Xie <bing708@gmail.com>
2018-08-28 16:55:33 +1000

    Use the engine in main application

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_054131) do
+ActiveRecord::Schema.define(version: 2018_08_28_041923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blorgh_articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "author_id"
+  end
+
+  create_table "blorgh_comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "buy_in_stocks", force: :cascade do |t|
     t.string "stock_symbol", null: false
--- engines/blorgh/app/models/blorgh/article.rb
+++ engines/blorgh/app/models/blorgh/article.rb
@@ -1,5 +1,15 @@
 module Blorgh
   class Article < ApplicationRecord
+    attr_accessor :author_email
+
     has_many :comments
+    belongs_to :author, class_name: Blorgh.author_class.to_s
+
+    before_validation :set_author
+
+    private
+      def set_author
+        self.author = Blorgh.author_class.find_or_create_by(email: author_email)
+      end
   end
 end
