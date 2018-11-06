64fefeec443a9a710faaa80bec545d52dff2b8c4
Bing Xie <bing708@gmail.com>
2018-11-07 10:35:13 +1100

    Add require master key to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+  config.require_master_key = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
