d558e28291c163b27d948e9b0d2ea310fe12766a
Bing Xie <bing708@gmail.com>
2018-11-19 13:16:41 +1100

    enrich facebook login logic

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    # Check the OmniAuth docs and each omniauth-facebook gem's README to know which information is being returned.
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+
+    # https://github.com/plataformatec/devise/wiki/OmniAuth:-Overview
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
   end
 end
