747c2d6a65ecd95e2fac6f9837b010edad106e2c
Bing Xie <bing708@gmail.com>
2019-04-03 22:38:51 +1100

    Add market and positions news on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,14 @@ class WelcomeController < ApplicationController
   # :nocov:
   def index
     logger.info 'Welcome page!'
+
+    @market_news = IEX::Resources::News.get(:market, 10)
+    @stocks_news = []
+    LongTermStock.all.each do |stock|
+      news = IEX::Resources::News.get(stock.stock_symbol)
+      @stocks_news.push(*news)
+    end
+
     render :index
   end
   # :nocov:
